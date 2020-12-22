b76d4212f7ab15baaf6488fa8ec4be7636a6c535
Student <ali.muhandis.7@gmail.com>
2020-12-22 09:19:18 +0500

    created Type struct

---
+++ pkg/types/types.go
@@ -0,0 +1,28 @@
+package types
+
+type Money int64
+
+type PaymrntCategoty string
+
+type PaymentStatus string
+
+const (
+	PaymentStatusOk PaymentStatus = "OK"
+	PaymentStatusFail PaymentStatus = "FAIL"
+	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
+)
+
+type Payment struct {
+	ID string
+	Amount Money
+	Category PaymrntCategoty
+	Status PaymentStatus
+}
+
+type Phone string
+
+type Account struct {
+	ID int64
+	Phone Phone
+	Balance Money
+}

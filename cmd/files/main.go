15203c16414caa12971d4579e04a9151c1866c6e
Student <ali.muhandis.7@gmail.com>
2020-12-27 16:24:35 +0500

    learning new lection goroutin

--- cmd/files/main.go
+++ cmd/files/main.go
@@ -1,112 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"os"
-
-	"github.com/AlisherGulomzoda/wallet/pkg/wallet"
+	"sync"
 )
 
 
+
 func main() {
-	svc := &wallet.Service{}
+	log.Print("Main started!")
+	wg := sync.WaitGroup{}
+	wg.Add(2)
 
-	account, err := svc.RegisterAccount("+992935811031")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = svc.Deposit(account.ID, 10)
-	if err != nil {
-		switch err {
-		case wallet.ErrAccountNotFound:
-			fmt.Println(wallet.ErrAccountNotFound)
-		case wallet.ErrPhoneAlreadyRegitered:
-			fmt.Println(wallet.ErrPhoneAlreadyRegitered)
-		case wallet.ErrAmountMustGreateZero:
-			fmt.Println(wallet.ErrAmountMustGreateZero)
+	sum := 0
+	go func ()  {
+		defer wg.Done()
+		for i := 0; i < 1_000; i++ {
+			sum++
 		}
-		return
-	}
-
-	data := byte(0b0100_0111)
-	fmt.Println(int(data))
-	fmt.Println(string(data))
-
-	fmt.Println(account.Balance)
-
-	err = os.Chdir("cmd/files")
-	if err != nil {
-		log.Print(err)
-	}
-
-
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Print(wd)
-
-	file, err := os.Open("data/readme.txt")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	defer func () {		
-		if err := file.Close(); err != nil {
-			log.Print(err)
+	}()
+	go func ()  {
+		defer wg.Done()
+		for i := 0; i < 1_000; i++ {
+			sum++
 		}
 	}()
 
-	log.Printf("#%v", file)
-
-	content := make([]byte, 0)
-	buf := make([]byte, 4096)
-
-	for{
-		read, err := file.Read(buf)
-		if err == io.EOF {
-			content = append(content, buf[:read]...)
-			break
-		}
-
-		if err != nil {
-			log.Print(err)
-			return
-		}	
-		content = append(content, buf[:read]...)
-	}
-
-	dat := string(content)
-	log.Print(dat)
-
-	newFile, err := os.Create("data/message.txt")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	defer func (){
-		if cerr := newFile.Close(); cerr != nil {
-			log.Print(cerr)
-		}
-	}()
-
-	_, err = newFile.Write([]byte("Hello from GO!"))
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	err = svc.ExportToFile("accountData/accounts.txt")
-	if err != nil {
-		log.Print(err)
-		return
-	}
+	wg.Wait()
+	log.Print("Main finished!")
+	log.Print(sum)
 
 }
